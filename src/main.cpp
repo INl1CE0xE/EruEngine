2512f5af72a45fb24bf3e296945ec4e45765798e
INl1CE0x3 <Ibrashos@gmail.com>
2024-05-10 22:28:49 +0300

    Remake main file, for education

--- src/main.cpp
+++ src/main.cpp
@@ -5,46 +5,13 @@
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
+    if (!glfwInit()) {
         return -1;
     }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGL()) {
-        std::cout << "Can't load GLAD!";
-        return -1;
-    }
-
-    std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor;
-
-
-    glClearColor(0, 1, 0, 1);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return 0;
+
 }
