20595968421aeb11013d380181e5a9b22c69a18e
INl1CE0xE <ibrashikx@gmail.com>
2024-08-03 00:55:32 +0300

    add fragmentShader, add shaderProgram, add VBO and VAO.
    finally create normal trinagles with colors.

--- src/main.cpp
+++ src/main.cpp
@@ -22,11 +22,11 @@ const char* vertex_shader =
 "out vec3 color;"
 "void main() {"
 "   color = vertex_color;"
-"   gl_position = vec4(vertex_position, 1.0);"
+"   gl_Position = vec4(vertex_position, 1.0);"
 "}";
 
 const char* fragment_shader =
-"#version460 core\n"
+"#version 460 core\n"
 "in vec3 color;"
 "out vec4 frag_color;"
 "void main() {"
@@ -42,6 +42,7 @@ void processInput(GLFWwindow* window);
 int main()
 {
     if (!glfwInit()) {
+        std::cout << "glfwInit failed!" << std::endl;
         return -1;
     }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -54,6 +55,7 @@ int main()
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
 
@@ -61,30 +63,74 @@ int main()
         std::cout << "Failed to initialize GLAD";
         return -1;
     }
-    
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
 
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    int success; // лог для проверки шейдеров на компиляцию
+    char infolog[512];
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); // Вертекс шейдер
     glShaderSource(vertexShader, 1, &vertex_shader, NULL);
     glCompileShader(vertexShader);
-    int success;
-    char infolog[512];
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
+    
     if (!success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infolog);
         std::cout << "Error::Shader:Vertex:compilation_failed\n" << infolog<<std::endl;
     }
 
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER); // Фрагментный шейдер
+    glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infolog);
+        std::cout << "Error::Shader:Fragment:compilation_failed\n" << infolog << std::endl;
+    }
+
+    GLuint shaderProgram = glCreateProgram(); // Линкование шейдеров в одну шейдерную программу
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+        //Тут используются два VBO для точек и для цвета, чтобы от точек переливался цвет на треугольнике
+
+    GLuint VBO_points = 0;
+    glGenBuffers(1, &VBO_points); // Генерация буфера точек
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_points); // Привязка буфера точек к функции массива точек
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+    
+    GLuint VBO_colors = 0;
+    glGenBuffers(1, &VBO_colors);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_colors);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint VAO = 0;
+    glGenVertexArrays(1, &VAO); // Генерация массива для буферов
+    glBindVertexArray(VAO); // Работа с VAO
+    
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_points);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO_colors);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+   
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // glViewport func with resize
 
     while (!glfwWindowShouldClose(window)) { //Rendering
         processInput(window); // Key Input
 
-        glClearColor(0.4f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
